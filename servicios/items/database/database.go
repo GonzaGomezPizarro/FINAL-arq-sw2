2dd18f3ff4198f36fe1b91c81605778d2b19660d
gonzagomezp <1912988@ucc.edu.ar>
2024-01-31 16:54:59 -0300

    ACA TODO OK !!!!!!!!!!!!!11

--- servicios/items/database/database.go
+++ servicios/items/database/database.go
@@ -16,27 +16,30 @@ var (
 
 func init() {
 	// Configura la conexión a la base de datos
-	clientOptions := options.Client().ApplyURI("mongodb://127.0.0.1:27017")
+	log.Println("  > PANG <  ")
+	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
 
+	log.Println("  > PENG <  ")
 	// Agrega la credencial al objeto clientOptions
 	clientOptions.Auth = &options.Credential{
 		Username: "root",
 		Password: "CONTRASENA",
 	}
 
+	log.Println("  > PING <  ")
 	// Conecta con la base de datos
 	var err error
 	client, err = mongo.Connect(context.Background(), clientOptions)
 	if err != nil {
 		log.Fatal(err)
 	}
-
+	log.Println("  > PONG <  ")
 	// Comprueba la conexión
 	err = client.Ping(context.Background(), nil)
 	if err != nil {
 		log.Fatal(err)
 	}
-
+	log.Println("  > PUNG <  ")
 	// Establece la base de datos por defecto
 	database = client.Database("itemss")
 
